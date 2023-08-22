b484873910f8651a8c916f01c905bd098f59a82d
Rozmukhamedov Islom <workislom@gmail.comgit config --global user.name Rozmukhamedov>
2023-08-22 17:14:55 +0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
       <Header />
       <Main />
       <Feedback />
-      <Information />
+      {/* <Information /> */}
       <Tours />
       <Travel />
       <Work />
