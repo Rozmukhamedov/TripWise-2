78ef36ea841493d102171f41c36f086db4f22a11
Rozmukhamedov Islom <workislom@gmail.comgit config --global user.name Rozmukhamedov>
2023-08-26 16:54:36 +0500

    update: 40

--- app/container/header/Header.tsx
+++ app/container/header/Header.tsx
@@ -4,13 +4,14 @@ import React from "react";
 import classes from "./style.module.css";
 import { Container } from "@mantine/core";
 import Image from "next/image";
+import LogoImage from "@/app/assets/images/logo.png";
 
 function Header() {
   return (
     <div className={`${classes.header} absolute w-full z-20	`}>
       <Container size={"xl"}>
         <div className="flex justify-between items-center py-4">
-          <Image src="" alt="TripWise" />
+          <Image src={LogoImage} alt="TripWise" />
           <h4>
             <a href="tel:+998777774033">+998 (77) 777-40-33</a>
           </h4>
--- app/container/main/Main.tsx
+++ app/container/main/Main.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useRef } from "react";
+import React from "react";
 import classes from "./style.module.css";
 
 function Main() {
